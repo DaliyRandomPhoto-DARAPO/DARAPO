bf7d1b9675146b16bc5ce02751f170e854d0574e
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-08-15 13:53:26 +0900

    Feature : 앱 버전 자동 업데이트

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -11,7 +11,7 @@ export default {
   expo: {
     name: APP_NAME,
     slug: APP_NAME,
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
